d0593ba6ebb4513ac9fe8a65be7dcfee8680c6d5
Mrs Fred <fred@Mrsde-MacBook-Pro.local>
2018-11-13 14:36:21 +0800

    Updates

---
+++ precache-manifest.5022011ab1c34f3a1f728cf0053ac461.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "647b3f702e2302eb608a",
+    "url": "/Currency2.0/static/js/runtime~main.647b3f70.js"
+  },
+  {
+    "revision": "1254a77394b387357cac",
+    "url": "/Currency2.0/static/js/main.1254a773.chunk.js"
+  },
+  {
+    "revision": "e534bc35e99daee532cb",
+    "url": "/Currency2.0/static/js/1.e534bc35.chunk.js"
+  },
+  {
+    "revision": "1254a77394b387357cac",
+    "url": "/Currency2.0/static/css/main.c5312927.chunk.css"
+  },
+  {
+    "revision": "e534bc35e99daee532cb",
+    "url": "/Currency2.0/static/css/1.51dc4a31.chunk.css"
+  },
+  {
+    "revision": "82dbf4f52dd5ac29e70bd86265c72b8b",
+    "url": "/Currency2.0/index.html"
+  }
+];
